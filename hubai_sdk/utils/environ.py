5125510f7cdf2ecd570db9eba8bf3b8c356cb0bd
kkeroo <61207502+kkeroo@users.noreply.github.com>
2025-12-08 18:10:29 +0100

    Fix for windows.

--- hubai_sdk/utils/environ.py
+++ hubai_sdk/utils/environ.py
@@ -7,6 +7,39 @@ from loguru import logger
 from pydantic import model_validator
 from typing_extensions import Self
 
+
+def _get_password_win(
+    q: multiprocessing.Queue, service_name: str, username: str
+) -> None:
+    try:
+        result = keyring.get_password(service_name, username)
+        q.put(result)
+    except Exception as e:
+        logger.warning(f"Failed to get password from keyring. Use HUBAI_API_KEY from environment variable instead. You can do so by running `export HUBAI_API_KEY=<your_api_key>`. Error: {e}")
+        q.put(None)
+
+
+def get_password_with_timeout_win(
+    service_name: str, username: str, timeout: float = 5
+) -> str | None:
+    # if system is mac with arm, use direct keyring call
+    if platform.system() == "Darwin" and platform.machine() == "arm64":
+        return keyring.get_password(service_name, username)
+
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(
+        target=_get_password_win, args=(q, service_name, username)
+    )
+    p.start()
+    p.join(timeout)
+    if p.is_alive():
+        p.terminate()
+        p.join()
+        return None
+    if not q.empty():
+        return q.get()
+    return None
+
 def get_password_with_timeout(
     service_name: str, username: str, timeout: float = 5
 ) -> str | None:
@@ -42,8 +75,13 @@ class Environ(BaseEnviron):
     @model_validator(mode="after")
     def validate_hubai_api_key(self) -> Self:
 
+        keyring_api_key = None
+
         with suppress(Exception):
-            keyring_api_key = get_password_with_timeout("HubAI", "api_key")
+            if platform.system() == "Windows":
+                keyring_api_key = get_password_with_timeout_win("HubAI", "api_key")
+            else:
+                keyring_api_key = get_password_with_timeout("HubAI", "api_key")
 
         if keyring_api_key:
             if self.HUBAI_API_KEY:
